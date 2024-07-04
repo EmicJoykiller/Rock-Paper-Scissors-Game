b7fc632189665fa39a05ff0022b90c945ad03c8c
nikan <nikanemami7@gmail.com>
2024-07-04 10:40:50 -0400

    Update script.js
    task achieved

--- script.js
+++ script.js
@@ -4,17 +4,31 @@ function getRandomComputerResult() {
     return options[randomIndex];
 }
 
+let playerScore = 0;
+let computerScore = 0;
 
 function hasPlayerWonTheRound(player, computer) {
-    if ((player === "Rock" && computer === "Scissors") ||
+    return (
+        (player === "Rock" && computer === "Scissors") ||
         (player === "Scissors" && computer === "Paper") ||
-        (player === "Paper" && computer === "Rock")) {
-        return true;
+        (player === "Paper" && computer === "Rock")
+    );
+}
+function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+        playerScore++;
+        return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (userOption === computerResult) {
+        return `It's a tie! Both chose ${userOption}`;
     } else {
-        return false;
-    }
+        computerScore++;
+        return `Computer wins! ${computerResult} beats ${userOption}`;
+    };
+
 
 }
 
-console.log(hasPlayerWonTheRound("Rock", "Scissors"));
-console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
